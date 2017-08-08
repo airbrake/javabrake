7db9d7be7f601c069a68cca38eb06d483037fb18
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2017-08-08 12:44:56 +0300

    More tests

--- src/test/java/NotifierTest.java
+++ src/test/java/NotifierTest.java
@@ -5,17 +5,11 @@ import static org.junit.Assert.*;
 import java.io.IOException;
 
 public class NotifierTest {
-  @Test
-  public void testReportSync() {
-    Notifier notifier = new Notifier(108686, "9fd45149aa4a6fc847a65a4c3f909208");
-    notifier.addFilter(
-        (Notice notice) -> {
-          notice.setContext("environment", "test");
-          return notice;
-        });
+  Notifier notifier = new Notifier(0, "");
 
-    Notice notice = notifier.reportSync(new IOException("hello from Java"));
-    assertNotNull(notice.id);
+  @Test
+  public void testBuildNotice() {
+    Notice notice = notifier.buildNotice(new IOException("hello from Java"));
 
     assertEquals(notice.errors.size(), 1);
     AirbrakeError err = notice.errors.get(0);
@@ -23,11 +17,36 @@ public class NotifierTest {
     assertEquals("hello from Java", err.message);
 
     AirbrakeStackRecord record = err.backtrace.get(0);
-    assertEquals("testReportSync", record.function);
+    assertEquals("testBuildNotice", record.function);
     assertEquals("test/javabrake/NotifierTest.class", record.file);
-    assertEquals(17, record.line);
+    assertEquals(12, record.line);
 
     String hostname = (String) notice.context.get("hostname");
     assertTrue(hostname != "");
+  }
+
+  @Test
+  public void testFilterData() {
+    notifier.addFilter(
+        (Notice notice) -> {
+          notice.setContext("environment", "test");
+          return notice;
+        });
+
+    Notice notice = notifier.buildNotice(new IOException("hello from Java"));
+
+    String env = (String) notice.context.get("environment");
+    assertEquals("test", env);
+  }
+
+  @Test
+  public void testFilterNull() {
+    notifier.addFilter(
+        (Notice notice) -> {
+          return null;
+        });
+
+    Notice notice = notifier.buildNotice(new IOException("hello from Java"));
+    assertNull(notice);
   }
 }
