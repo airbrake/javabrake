ee31c155e79fb3469276d1fe36868bb7108d8e50
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2017-09-15 16:07:40 +0300

    Bump version

--- src/main/java/Notice.java
+++ src/main/java/Notice.java
@@ -14,7 +14,7 @@ public class Notice {
   static {
     notifierInfo = new HashMap<>();
     notifierInfo.put("name", "javabrake");
-    notifierInfo.put("version", "0.1.3");
+    notifierInfo.put("version", "0.1.4");
     notifierInfo.put("url", "https://github.com/airbrake/javabrake");
   }
 
